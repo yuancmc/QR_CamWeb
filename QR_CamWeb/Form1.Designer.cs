348a8b024416dbd6e682d080fa2b9d857757bb1a
Yuanhua C.M. <chiuyuanhua@gmail.com>
2024-11-13 19:32:52 -0600

    Agregar archivos de proyecto.

---
+++ QR_CamWeb/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace QR_CamWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(218, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(335, 335);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ QR_CamWeb/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using BasselTech_CamCapture;
+
+namespace QR_CamWeb
+{
+    public partial class Form1 : Form
+    {
+        Camera cam;
+        Timer t;
+        BackgroundWorker worker;
+        Bitmap CapImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            t = new Timer();
+            cam = new Camera (pictureBox1);
+            worker = new BackgroundWorker();
+
+            worker.DoWork += Worker_Dowork;
+            t.Tick += T_Tick;
+            t.Interval = 1;
+
+
+        }
+
+        private void  T_Tick(object sender, EventArgs e)
+        {
+            CapImage = cam.GetBitmap();
+            if (CapImage != null && !worker.IsBusy)
+                worker.RunWorkerAsync();
+        }
+
+        private void Worker_Dowork(object sender, DoWorkEventArgs e)
+        {
+            QRCodeDecoder Decoder = new QRCodeDecoder();
+
+            try
+            {
+                // Decodificar el código QR
+                string decodedText = Decoder.decode(new QRCodeBitmapImage(CapImage));
+
+                // Comprobar si el texto decodificado es "Camila" o "Martina"
+                if (decodedText == "Camila" || decodedText == "Martina")
+                {
+                    // Detener el temporizador y el proceso de captura para evitar múltiples aperturas de Form2
+                    this.Invoke((MethodInvoker)delegate {
+                        t.Stop();
+                        cam.Stop();
+                        worker.Dispose();
+
+                        // Cambiar a Form2
+                        Form2 form2 = new Form2();
+                        form2.Show();
+                        this.Hide();  
+                    });
+                }
+                else
+                {
+                    // Mostrar mensaje de QR incorrecto
+                    this.Invoke((MethodInvoker)delegate {
+                        MessageBox.Show("QR incorrecto");
+                    });
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cam.Start();
+                t.Start();
+                button2.Enabled = true;
+                button1.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                cam.Stop();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cam.Stop();
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+
+}
